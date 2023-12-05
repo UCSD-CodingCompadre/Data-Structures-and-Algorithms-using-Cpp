d819c0eb09fb118c66e25d7c5b5d1527104fff10
CodingCocho <raulujr@gmail.com>
2023-12-04 23:46:38 -0800

    Added bubble sort notes.

--- sorting-algorithms/bubble-sort.cpp
+++ sorting-algorithms/bubble-sort.cpp
@@ -2,30 +2,6 @@
 #include <vector>
 
 /*
-This is bubble sort a sorting algorithm
-@param vector<int> nums the vector we are going to sort
-@return void
-*/
-void bubbleSort(std::vector<int>& nums, bool isDescending)
-{
-    for(int counter1 = 0; counter1 < nums.size()-1; counter1++)
-    {
-        for(int counter2 = counter1+1; counter2 < nums.size()-counter1-1; counter2++)
-        {
-            if(isDescending)
-            {
-                if(isLess(nums[counter1], nums[counter2])) swap(nums, counter1, counter2);
-            }
-            else
-            {
-                if(isGreater(nums[counter1], nums[counter2])) swap(nums, counter1, counter2);
-            }
-        }
-    }
-    return;
-}
-
-/*
 Helper method to check if a number is greater than the next number
 @param int num1 the first number used to compare
 int num2 the second number used to compare
@@ -72,13 +48,82 @@ void swap(std::vector<int>& nums, int index1, int index2)
     return;
 }
 
+/*
+This is bubble sort a sorting algorithm
+@param vector<int> nums the vector we are going to sort
+bool isDescending true if the sorting results in an descending vector
+@return void
+*/
+void bubbleSort(std::vector<int>& nums, bool isDescending)
+{
+
+    // Outer loop for the amount of comparisons made
+    for(int counter1 = 0; counter1 < nums.size() - 1; counter1++)
+    {
+        // Hold an early termination flag
+        bool flag = true;
+        
+        // Inner loop that moves the next number in the correct index
+        for(int counter2 = 0; counter2 < nums.size() - counter1 - 1; counter2++)
+        {
+
+            // Check if the sorting is descending
+            if(isDescending)
+            {
+
+                // Check if we need to swap
+                if(isLess(nums[counter2], nums[counter2+1])) 
+                {
+
+                    // Turn off the termination flag
+                    flag = false;
+
+                    // Swap the indexes
+                    swap(nums, counter2, counter2+1);
+                }
+            }
+
+            // Else the sorting is ascending
+            else
+            {
+
+                // Check if we need to swap
+                if(isGreater(nums[counter2], nums[counter2+1])) 
+                {
+
+                    // Turn off the termination flag
+                    flag = false;
+
+                    // Swap the indexes
+                    swap(nums, counter2, counter2+1);
+                }
+            }
+        }
+
+        // Check if we can terminate early
+        if(flag) return;
+    }
+    return;
+}
+
 int main ()
 {
     std::vector<int> example1 = {4, 8, 9, 2, 0, 11, 16, 3, 6};
+    std::vector<int> example2 = {7, 1, 9, 4, 5, 0, 11, 3};
     bubbleSort(example1, true);
+    bubbleSort(example2, false);
     
+    std::cout << "Example 1 descending order: ";
     for(int value : example1)
     {
-        std::cout << value << std::endl;
+        std::cout << value << ", ";
+    }
+
+    std::cout << '\n';
+
+    std::cout << "Example 2 ascending order: ";
+    for(int value : example2)
+    {
+        std::cout << value << ", ";
     }
 }
