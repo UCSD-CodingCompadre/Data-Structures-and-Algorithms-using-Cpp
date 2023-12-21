ece810c1e0e2bc082ee871a84ab2d828ca8db556
CodingCocho <raulujr@gmail.com>
2023-12-20 16:44:13 -0800

    Added Queue notes

--- data-structures/Queue.cpp
+++ data-structures/Queue.cpp
@@ -1,6 +1,34 @@
 #include <iostream> 
-#include <stack>
+#include <queue>
 
 /*
-This is a stack that follows a Last-In-First-Out sorting order.
+This is a queue that follows a First-In-First-Out sorting order.
 */
+
+int main()
+{
+
+    // Hold an example stacks
+    std::queue<int> MyQueue;
+
+    // Push elements
+    MyQueue.push(1);
+    MyQueue.push(2);
+    MyQueue.push(3);
+    MyQueue.push(4);
+    MyQueue.push(5);
+
+    // Print queue size
+    std::cout << "queue size: " << MyQueue.size() << std::endl;
+
+    // Loop until queue is empty
+    while(!MyQueue.empty())
+    {
+
+        // Print the element at the top of the queue
+        std::cout << MyQueue.front() << std::endl;
+
+        // Pop the top element from the queue
+        MyQueue.pop();
+    }
+}
